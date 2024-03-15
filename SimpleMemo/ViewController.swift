defcb4229765617a79f88e8262abb5977d684a57
seongmind <62093201+seongmind@users.noreply.github.com>
2024-03-15 11:33:23 +0900

    Xcode project 생성

---
+++ SimpleMemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SimpleMemo
+//
+//  Created by 김성민 on 3/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
